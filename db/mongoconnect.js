582d46897ba125398e459f23afc184e2efa4b04b
Vadim <vadimslip@gmail.com>
2022-11-14 11:30:34 +0200

    first

---
+++ db/mongoconnect.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { config } = require('../config/secret');
+
+main().catch(err => console.log(err));
+
+async function main() {
+await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.jzl8ly1.mongodb.net/Cluster0`);
+  console.log("mongo connect black22")
+  // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
+}
