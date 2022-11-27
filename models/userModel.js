88878acb69012a9d9f6dc1a6f126d87442182ba9
Alex <rozenbaum1988@gmail.com>
2022-11-27 11:46:50 +0200

    27_11_22

--- models/userModel.js
+++ models/userModel.js
@@ -35,8 +35,8 @@ let userSchema = new mongoose.Schema({
 
 exports.UserModel = mongoose.model("users", userSchema);
 
-exports.createToken = (_id, role,active) => {
-    let token = jwt.sign({ _id, role, active}, config.tokenSecret, { expiresIn: "1440mins" });
+exports.createToken = (_id,role,active) => {
+    let token = jwt.sign({ _id,role,active}, config.tokenSecret, { expiresIn: "1440mins" });
     return token;
 }
 
